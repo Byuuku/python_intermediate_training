15759174bdefb92c5f53230ec2077856dacda238
pawelskorka <pawelskorka1@gmail.com>
2021-01-31 13:18:54 +0100

    added base sound to class Cat, added for loop to created cat_objects

--- sda_exercises_oop_1/ex01-10/cat.py
+++ sda_exercises_oop_1/ex01-10/cat.py
@@ -1,6 +1,7 @@
 class Cat:
-    def __init__(self, name: str):
+    def __init__(self, name: str, sound: str = "meow, meow"):
         self.name = name
+        self.sound = sound
 
     def make_sound(self) -> str:
-        return f"Cat named {self.name} does 'meowww meowww meowwwwww'"
+        return f"Cat named {self.name} makes sound: '{self.sound}'"
--- sda_exercises_oop_1/ex01-10/main.py
+++ sda_exercises_oop_1/ex01-10/main.py
@@ -3,8 +3,15 @@ from cat import Cat
 
 def main():
     cat_object = Cat("Bonifacy")
-    cat_sound: str = cat_object.make_sound()
-    print(cat_sound)
+    cat_object_2 = Cat("Doodley", "mrrr, mrrr")
+    cat_object_3 = Cat("Mruczyslaw")
+    cat_object_4 = Cat("Dandy")
+
+    cats = [cat_object, cat_object_2, cat_object_3, cat_object_4]
+
+    for cat in cats:
+        cat_sound = cat.make_sound()
+        print(cat_sound)
 
 
 if __name__ == "__main__":
