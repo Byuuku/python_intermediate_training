08796952c5e9ceea2a0637aa220edb1c2853e944
pawelskorka <pawelskorka1@gmail.com>
2021-02-06 12:32:20 +0100

    figures update

---
+++ sda_exercises_oop_1/ex11-13/main.py
@@ -0,0 +1,21 @@
+from figures import *
+
+
+def main():
+    circle1 = Circle(5)
+    circle2 = Circle(10)
+    circle3 = Circle(15)
+    triangle1 = Triangle(15, 5)
+    triangle2 = Triangle(20, 10)
+    triangle3 = Triangle(10, 5)
+    rectangle1 = Rectangle(35, 5)
+    rectangle2 = Rectangle(45, 5)
+    rectangle3 = Rectangle(50, 5)
+
+    print(circle1.get_area())
+    print(triangle2.get_area())
+    print(rectangle3.get_area())
+
+
+if __name__ == "__main__":
+    main()
