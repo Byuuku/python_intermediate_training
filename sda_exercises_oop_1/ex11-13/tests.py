3d74ddf6b5c2c465e3a934a9a3fb3ebf6701a913
pawelskorka <pawelskorka1@gmail.com>
2021-02-06 13:57:06 +0100

    test added

---
+++ sda_exercises_oop_1/ex11-13/tests.py
@@ -0,0 +1,15 @@
+import pytest
+from figures import Figures, Circle, Triangle, Rectangle
+
+
+def test_check_area():
+    #  given
+    circle1 = Circle(1)
+    triangle1 = Triangle(2, 1)
+    rectangle1 = Rectangle(2, 2)
+
+    #  when
+    result = Figures.count_area([circle1, triangle1, rectangle1])
+
+    #  then
+    assert result == 8.14
