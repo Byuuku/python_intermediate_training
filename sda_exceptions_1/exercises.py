5be73bf5aed834c9f9e8f294b49672e795e4d08d
pawelskorka <pawelskorka1@gmail.com>
2021-02-07 10:19:30 +0100

    case1 and case 2 exceptions added

--- sda_exceptions_1/exercises.py
+++ sda_exceptions_1/exercises.py
@@ -1,5 +1,24 @@
 def case_1():
     list_of_numbers = [1, 5, 8, 10, 21]
     print("case_1_before")
-    result = list_of_numbers[5]
+
+    # first method
+    try:
+        result = list_of_numbers[5]
+    except IndexError as in_exc:
+        print(f"M1 - Index exception: {in_exc.args} caught")
+    except Exception as exc:
+        print(f"M1 - Exception: {exc.args} caught")
     print("case_1_after")
+
+    # second method
+    try:
+        result = list_of_numbers[5]
+    except (IndexError, Exception) as exc:
+        print(f"M2 - Exception: {exc.args} caught")
+
+
+def case_2(name: str):
+    if len(name) <= 0:
+        raise ValueError("String is empty")
+    print(f"Given name is : {name}")
--- sda_exceptions_1/main.py
+++ sda_exceptions_1/main.py
@@ -1,11 +1,17 @@
-from exercises import case_1
+from exercises import case_1, case_2
 
 
 def main():
-    print("Start up")
-    case_1()
-    print("Finish")
-    pass
+    # print("Start up c1")
+    # case_1()
+    # print("Finish c1")
+
+    print("Start up c2")
+    try:
+        case_2("")
+    except ValueError as ve:
+        print(f"M2 - Value exception: {ve.args} caught")
+    print("Finish c2")
 
 
 if __name__ == "__main__":
