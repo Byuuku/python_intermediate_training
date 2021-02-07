d0b6b573582ae5d67436e0c64a8d56531f0f9e68
pawelskorka <pawelskorka1@gmail.com>
2021-02-07 13:15:45 +0100

    pickle write-read created

--- sda_serialize_1/main.py
+++ sda_serialize_1/main.py
@@ -1,9 +1,12 @@
-from pickle_training import pickle_write
+from pickle_training import pickle_write, pickle_read
 
 
 def main():
     new_list = ["Butter", "Bread", "Water", "Soda"]
     pickle_write(new_list)
+
+    list_of_strings = pickle_read()
+    print(list_of_strings)
 
 
 if __name__ == "__main__":
