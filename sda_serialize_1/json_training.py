0a3108b7f4b9bd779fbe703b60ca3c2fd396a66f
pawelskorka <pawelskorka1@gmail.com>
2021-02-07 18:32:41 +0100

    json read-write added

--- sda_serialize_1/json_training.py
+++ sda_serialize_1/json_training.py
@@ -1,2 +1,23 @@
 import json
 
+
+def json_write(items: list):
+    print("json_write Start")
+    try:
+        with open("./objects.json", "w") as fd:
+            json.dump(items, fd, indent=2)
+    except (IOError, Exception) as e:
+        print(f"Exception while writing JSON format info {e.args}")
+    print("json_write Finish")
+
+
+def json_read():
+    data_list = []
+    print("json_read Start")
+    try:
+        with open("./objects.json", "r") as fd:
+            data_list = json.load(fd)
+    except (IOError, Exception) as e:
+        print(f"Exception while reading JSON format info {e.args}")
+    print("json_read Finish")
+    return data_list
--- sda_serialize_1/main.py
+++ sda_serialize_1/main.py
@@ -1,5 +1,6 @@
 from pickle_training import pickle_write, pickle_read
 from csv_training import csv_write, csv_read
+from json_training import json_write, json_read
 
 
 def main():
@@ -9,15 +10,43 @@ def main():
     # list_of_strings = pickle_read()
     # print(list_of_strings)
 
-    new_users = [
-        ("John", "Smith", 93121323542),
-        ("Gary", "Youngman", 99062090756),
-        ("Agatha", "Brown", 88061245692)
+    # new_users = [
+    #     ("John", "Smith", 93121323542),
+    #     ("Gary", "Youngman", 99062090756),
+    #     ("Agatha", "Brown", 88061245692)
+    # ]
+    #
+    # csv_write(new_users)
+    # users_list = csv_read()
+    # print(users_list)
+
+    new_objects = [
+        {
+            "object": "Line",
+            "points": 0,
+            "lines": 1
+        },
+        {
+            "object": "Circle",
+            "points": 0,
+            "lines": 1
+        },
+        {
+            "object": "Triangle",
+            "points": 3,
+            "lines": 3
+        },
+        {
+            "object": "Square",
+            "points": 4,
+            "lines": 3
+        }
     ]
 
-    csv_write(new_users)
-    users_list = csv_read()
-    print(users_list)
+    json_write(new_objects)
+
+    data_list = json_read()
+    print(data_list)
 
 
 if __name__ == "__main__":
