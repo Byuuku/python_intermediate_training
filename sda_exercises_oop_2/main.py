908c90762fde6ecb6ebccfc7145e4a15a65fc6b9
pawelskorka <pawelskorka1@gmail.com>
2021-02-15 20:39:50 +0100

    main change

--- sda_exercises_oop_2/main.py
+++ sda_exercises_oop_2/main.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 from person import Person
 from employee import Employee
 from manager import Manager
 
-def main():
 
-    person = Person("Jan", "Kowalski", )
+def main():
+    employee = Employee("Jan", "Kowalski", date(1985, 12, 21))
+
+    print(employee.birth_date)
 
 
 if __name__ == "__main__":
