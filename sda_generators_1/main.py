201098ad6e3615ed1295772661bfd1ae1f389534
pawelskorka <pawelskorka1@gmail.com>
2021-02-20 15:29:14 +0100

    case 4-5?

--- sda_generators_1/main.py
+++ sda_generators_1/main.py
@@ -1,15 +1,21 @@
 from sda_generators_1.case_1 import iterator_ex_1
 from sda_generators_1.case_2 import iterator_ex_2
 from sda_generators_1.case_3 import iterator_ex_3
+from sda_generators_1.case_4_5 import generator_ex_4, reader_line_by_line
 
 
 def main():
     # iterator_ex_1()
-    print("Exercise 2 results:")
-    iterator_ex_2(10000)
-    print("\n")
-    print("Exercise 3 results:")
-    iterator_ex_3(10000)
+
+    # print("Exercise 2 results:")
+    # iterator_ex_2(10000)
+    # print("\n")
+    # print("Exercise 3 results:")
+    # iterator_ex_3(10000)
+
+    # generator_ex_4()
+
+    reader_line_by_line()
 
 
 if __name__ == "__main__":
