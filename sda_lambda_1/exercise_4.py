8c91051048d897b36dbe37e57035afcefdb937fa
Pawel Skorka <pawelskorka1@gmail.com>
2021-02-21 13:02:44 +0100

    Delete sda_lambda_1 directory

--- sda_lambda_1/exercise_4.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-
-def exercise_4():
-    randoms_list = [randint(0, 101) for item in range(10)]
-    print("Before")
-    print(randoms_list)
-    print("After")
-    print(list(map(lambda number: number * 2, randoms_list)))
